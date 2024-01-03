e1e5ed9df41b3ba2b08244e6185f0be2a26ae6d5
Aroita <aroitaim84@gmail.com>
2024-01-03 12:51:44 +0100

    generar id auto

---
+++ Pildoras_Informaticas/src/poo/Const_uso_final_ID.java
@@ -0,0 +1,59 @@
+package poo;
+
+//LA CONTANTE SE ASEGURA QUE LA PALABRA RESERVADA: FINAL, NO CAMBIE EL VALOR
+// private static int IdSiguiente=1, aplica un id que no se puede modificar es generado auto
+
+public class Const_uso_final_ID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Empleados trabajador1=new Empleados("Paco"); //2 objetos trabajador1y2 con estado inicial de empleado sera el string que le pasamos y la seccion la completa en el constructor
+		Empleados trabajador2=new Empleados("Ana");
+		
+		trabajador1.cambiaSeccion("RRHH");
+		//trabajador1.cambiaNombre("maria"); //PRUEBA CON LA PALABRA RESERVADA FINAL
+		
+		System.out.println(trabajador1.devuelveDatos()); //el metodo getter retornara los datos
+		System.out.println(trabajador2.devuelveDatos());
+
+	}
+
+}
+
+// 1º CREAMOS LA CLASE empleado que sea capaz de construir instancias u objetos a la que tenga un nombre empleado y una seccion de cada trabajador.
+class Empleados{
+	
+	//1º encapsulacion
+	//private final String nombre;  //CONSTANTE, PRUEBA CON LA PALABRA RESERVADA FINAL
+	private  String nombre;
+	private String seccion;
+	private int Id;
+	private static int IdSiguiente=1;
+	
+//2º EL CONSTRUCTOR, inicializa las variables(instancias del objeto)
+	public Empleados(String nom) {
+		nombre=nom;
+		seccion="Administración";
+		Id=IdSiguiente;
+		IdSiguiente++;
+
+	}
+	
+	//METODO GETTER
+	public String devuelveDatos() {
+		return "Empleado: " + nombre + ", y la secciones es: "+ seccion + " id:" + Id;
+	}
+	
+	            //CONSTANTE, PRUEBA CON LA PALABRA RESERVADA FINAL
+	public void cambiaNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	
+	
+	//METODO SETTER(modifica la seccion y no tienen return)
+	public void cambiaSeccion(String seccion) {
+		this.seccion=seccion;
+	}
+
+}
